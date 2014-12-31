ccd70e84cae0a3ea5af77cdb09b85d21d6a05009
Mike Vardy <mike.vardy@gmail.com>
2014-12-31 11:54:52 -0500

    Iniital Commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name profitmakerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the profitmakerApp
+ */
+angular.module('profitmakerApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
